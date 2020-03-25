35b711242526cfa9b0792306d5396cf0d30e485e
liubing02 <liubing02@finupgroup.com>
2020-03-25 15:02:05 +0800

    window + aggregate + partitionCustom的应用, 添加注释, 修改bug

--- flink19/src/main/scala/com/lb/flink19/window/AggregateFunctionExample.scala
+++ flink19/src/main/scala/com/lb/flink19/window/AggregateFunctionExample.scala
@@ -3,8 +3,13 @@ package com.lb.flink19.window
 import org.apache.flink.api.common.functions.{AggregateFunction, Partitioner}
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.slf4j.LoggerFactory
+import org.apache.flink.api.scala._
 
-case  class Ab(a: String, b: Long)
+case class Ab(a: String, b: Long)
+// key, 最大值, 最小值, 总数, 个数
+case class AggDemoBean(s: String, max: Long, min: Long, sum: Long, num: Long)
+
 
 /**
   * @ClassName AggregateFunctionExample
@@ -13,63 +18,71 @@ case  class Ab(a: String, b: Long)
   * @Date 2020/1/9 15:23
   * @Version 1.0
   **/
-object AggregateFunctionExample  extends  App{
+object AggregateFunctionExample extends App {
+
+  val log = LoggerFactory.getLogger(AggregateFunctionExample.getClass)
 
   val env = StreamExecutionEnvironment.getExecutionEnvironment
   // nc -l 9000
+
+  /**
+    * nc -l 9000
+    * 数据格式:  key,num  [a,20]
+    */
   val ds: DataStream[String] = env.socketTextStream("127.0.0.1", 9000)
 
-  import org.apache.flink.api.scala._
 
-  ds.partitionCustom(new CustomPartitioner, ("_"))
+  ds.map(Tuple1[String](_))  // partitionCustom 必须使用Tuples, POJOs, case classes, etc格式, 否则报错: This key operation requires a composite type such as Tuples, POJOs, case classes, etc
+    .partitionCustom(new CustomPartitioner, 0)  // (分区函数, key取值下标)
     .map { x =>
-    val v = x.split(",", -1)
-    (v(0), v(1).toLong)
-  }.keyBy(_._1).timeWindow(Time.seconds(10)).aggregate(new TestAggregateFunction).print()
+      println(x._1)
+      val v = x._1.split(",", -1)
+      Ab.apply(v(0), v(1).toLong)
+    }.keyBy(_.a).timeWindow(Time.seconds(10)).aggregate(new TestAggregateFunction).print()
 
-   env.execute()
+  env.execute("123")
 }
 
 // 自定义分区器，将不同的Key(用户ID)分到指定的分区
 // key: 根据key的值来分区
 // numPartitions: 下游算子并行度
-  class CustomPartitioner extends Partitioner[String]{
-    override def partition(k: String, i: Int): Int = {
-        k match {
-          case "a" => 0
-          case "b" => 0
-          case _ => i
-        }
+class CustomPartitioner extends Partitioner[String] {
+  override def partition(key: String, numPartitions: Int): Int = {
+    println(numPartitions+"-------------------")
+    key match {
+      case "a" => 0
+      case "b" => 1
+      case _ => numPartitions - 1  //numPartitions为当前线程数, local默认cpu数.  分区存储在数值中, 取值下标需-1, 否则报数组越界异常
     }
   }
+}
 
-// key, 最大值, 最小值, 总数, 个数
-case class AggDemoBean(s: String, max: Long, min: Long, sum: Long, num: Long)
 
-class TestAggregateFunction extends  AggregateFunction[(String, Long), (AggDemoBean), (String, Long, Long, Long, Long, Double) ]{
+class TestAggregateFunction extends AggregateFunction[Ab, AggDemoBean, (String, Long, Long, Long, Long, Double)] {
 
   // 每一条输入数据，和迭代数据如何迭代
-  override def add(in: (String, Long), acc: (AggDemoBean)): (AggDemoBean) = {
-    print(in._1) ; println(in._2)
-    AggDemoBean.apply(in._1, if(acc.max > in._2) acc.max else in._2 , if(acc.min < in._2 &&  acc.min > 0) acc.min else in._2 , acc.sum + in._2, acc.num + 1)
+  override def add(in: Ab, acc: (AggDemoBean)): (AggDemoBean) = {
+    print(in.a)
+    println(in.b)
+    AggDemoBean.apply(in.a, if (acc.max > in.b) acc.max else in.b, if (acc.min < in.b && acc.min > 0) acc.min else in.b, acc.sum + in.b, acc.num + 1)
   }
 
   // 迭代状态的初始值
-  override def createAccumulator(): (AggDemoBean) = AggDemoBean.apply("", 0l , 0l, 0L,  0l)
+  override def createAccumulator(): (AggDemoBean) = AggDemoBean.apply("", 0l, 0l, 0L, 0l)
 
 
   // 返回数据，对最终的迭代数据如何处理，并返回结果
   // 返回(key, 最大值, 最小值, 总数, 个数, 平均数)
   override def getResult(acc: (AggDemoBean)): (String, Long, Long, Long, Long, Double) = {
-    ( acc.s, acc.max, acc.min, acc.sum, acc.num,  acc.sum / acc.num)
+    (acc.s, acc.max, acc.min, acc.sum, acc.num, acc.sum / acc.num)
   }
 
   // 多个分区的迭代数据如何合并
   override def merge(a: (AggDemoBean), b: (AggDemoBean)): (AggDemoBean) = {
     println("----------------")
     AggDemoBean.apply(s"${a.s}|${b.s}",
-      if(a.max > b.max) a.max else b.max,
-      if(a.min < b.min) a.min else b.min,
+      if (a.max > b.max) a.max else b.max,
+      if (a.min < b.min) a.min else b.min,
       a.sum + b.sum,
       a.num + b.num
     )
